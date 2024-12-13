8e50565e4b25d52c49ae5b0bce522dd266ba0518
injili <nyarekigospel@gmail.com>
2024-12-13 04:34:43 +0300

    Certs section optimized

--- src/components/certs.jsx
+++ src/components/certs.jsx
@@ -6,12 +6,12 @@ import five from "../assets/img/award5.png";
 
 export default function Certs() {
   return (
-    <div className="flex bg-primary gap-12 py-24 justify-center items-center">
-      <img src={one} alt="" className="h-28 " />
-      <img src={two} alt="" className="h-28" />
-      <img src={three} alt="" className="h-28" />
-      <img src={four} alt="" className="h-28" />
-      <img src={five} alt="" className="h-28" />
+    <div className="flex flex-wrap bg-primary gap-4 xl:gap-12 px-4 py-8 md:py-12 lg:py-16 xl:py-24 justify-center items-center">
+      <img src={one} alt="" className="h-12 md:h-16 lg:h-24 xl:h-28" />
+      <img src={two} alt="" className="h-12 md:h-16 lg:h-24 xl:h-28" />
+      <img src={three} alt="" className="h-12 md:h-16 lg:h-24 xl:h-28" />
+      <img src={four} alt="" className="h-12 md:h-16 lg:h-24 xl:h-28" />
+      <img src={five} alt="" className="h-12 md:h-16 lg:h-24 xl:h-28" />
     </div>
   );
 }
